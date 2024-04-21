27146224b878ea89e8e0d606a02602a46d4e53d4
MichaelHinrichs <mchinrichs@gmail.com>
2024-04-21 15:13:39 -0500

    Extract all files
    Crawl through KXR files to search for the beginning of zlib files. Closes #2.

--- Program.cs
+++ Program.cs
@@ -12,10 +12,36 @@ namespace KXR_Decompresser
             if (new string(br.ReadChars(4)) != "kxrf")
                 throw new System.Exception("Not a KXR file.");
 
-            br.BaseStream.Position = 50;
-            using FileStream FS = File.Create(Path.GetDirectoryName(args[0]) + "//" + Path.GetFileNameWithoutExtension(args[0]));
-            using (var ds = new DeflateStream(new MemoryStream(br.ReadBytes((int)(br.BaseStream.Length - 50))), CompressionMode.Decompress))
-                ds.CopyTo(FS);
+            br.BaseStream.Position = 47;
+            byte[] search = br.ReadBytes(3);
+            string searchString;
+            System.Collections.Generic.List<long> starts = new();
+            while (br.BaseStream.Position < br.BaseStream.Length)
+            {
+                searchString = System.Text.Encoding.ASCII.GetString(search);
+                if (System.Text.RegularExpressions.Regex.Match(searchString, @"\0x\^").Success)
+                    starts.Add(br.BaseStream.Position);
+
+                br.BaseStream.Position -= 2;
+                search = br.ReadBytes(3);
+            }
+
+            string path = Path.GetDirectoryName(args[0]) + "//" + Path.GetFileNameWithoutExtension(args[0]);
+            Directory.CreateDirectory(path);
+
+            for (int i = 0; i < starts.Count - 1; i++)
+            {
+                br.BaseStream.Position = starts[i];
+                if (i == 186)
+                    i = 186;
+
+                using (var ds = new DeflateStream(new MemoryStream(br.ReadBytes((int)((starts[i + 1] - 2) - starts[i]))), CompressionMode.Decompress))
+                    ds.CopyTo(File.Create(path + "//" + i));
+            }
+
+            br.BaseStream.Position = starts[^1];
+            using (var ds = new DeflateStream(new MemoryStream(br.ReadBytes((int)(br.BaseStream.Length - starts[^1]))), CompressionMode.Decompress))
+                ds.CopyTo(File.Create(path + "//" + (starts.Count - 1)));
         }
     }
 }
